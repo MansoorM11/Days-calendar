d2ce0e4f7c60ed28d6f60c18f90aa82aca09ccca
TzeMingHo <tzemingho@gmail.com>
2026-02-14 17:49:21 +0000

    created a repeat grid row 7 1fr at the calendar-body

--- web.mjs
+++ web.mjs
@@ -7,6 +7,6 @@ import { getGreeting } from "./common.mjs";
 import daysData from "./days.json" with { type: "json" };
 
 window.onload = function () {
-  document.querySelector("main").innerText =
+  document.getElementById("default-message").innerText =
     `${getGreeting()} - there are ${daysData.length} known days`;
 };

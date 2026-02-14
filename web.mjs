64e9141f8cba3494f610c2a9569e077108092eb0
TzeMingHo <tzemingho@gmail.com>
2026-02-14 16:44:36 +0000

    created a header and a navbar

--- web.mjs
+++ web.mjs
@@ -6,6 +6,7 @@
 import { getGreeting } from "./common.mjs";
 import daysData from "./days.json" with { type: "json" };
 
-window.onload = function() {
-    document.querySelector("body").innerText = `${getGreeting()} - there are ${daysData.length} known days`;
-}
+window.onload = function () {
+  document.querySelector("main").innerText =
+    `${getGreeting()} - there are ${daysData.length} known days`;
+};
